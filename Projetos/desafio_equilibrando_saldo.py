810ff9e76d3447e15f3a3b24df0c684f6e30b715
Gabriel Felipe <gabrielfelipe.oliveira2000@gmail.com>
2023-09-18 15:43:46 -0300

    Atualizando desafio equilibrando saldo usando elif.

--- Projetos/desafio_equilibrando_saldo.py
+++ Projetos/desafio_equilibrando_saldo.py
@@ -1,55 +1,62 @@
-saldo_atual = float(1000.00)
-mensagem_inicial = print((f"Olá, seja bem vindo ao banco Thor Peixinho & Jack Tubarão! Seu saldo atual é de: R$ {saldo_atual:.2f}!"))
-opcao = (input("No que podemos te ajudar hoje? \n[1]- Depósito [2]- Saque [3]- Empréstimo\n:" ))
-#Mensagem de boas vindas
-
-#primeiro bloco
-if opcao == "1":
-    deposito = float(input("Qual valor deseja depositar?\n:R$"))
-    saldo_deposito = saldo_atual + deposito
-    mensagem = input(f"Seu saldo atual é de: R${saldo_deposito:.2f}!\nDeseja realizar um saque? [1]- Sim [2]- Não\n:")
+saldo_inicial = 1000
+mensagem_inicial=int(input("Sejam bem vindos ao Banco Thor Peixinho & Jack Tubarão.\nNo que podemos te ajudar hoje?\n[1]- Depósito [2]- Saque [3]- Empréstimo\nDigite a opção: "))
+   #depósito
+if mensagem_inicial == 1:
+    valor_deposito = float(input(f"Seu saldo atual é de R${saldo_inicial:.2f}!\nQual valor deseja depositar?\nR$"))
+    if valor_deposito >0:
+        saldo_deposito = saldo_inicial+valor_deposito
+        print(f"Depósito realizado com sucesso! Seu saldo atual é de {saldo_deposito:.2f}\nDeseja realizar outra operação?.")
+    outras_operacoes = int(input("[1]- Depósito [2]- Saque [3]- Sair\n"))
+    if outras_operacoes == 1:
+            valor_deposito2=float(input(f"Seu saldo atual é de R${saldo_deposito:.2f}!\nQual valor deseja depositar?\nR$"))
+            if valor_deposito2 >0:
+                print(f"Depósito realizado com sucesso! Seu saldo atual é de {saldo_deposito+valor_deposito2:.2f}\nObrigado por utilizar nossos serviços.")
+    elif outras_operacoes == 2:
+        valor_saque2 = float(input(f"Seu saldo atual é de R${saldo_deposito:.2f}!\nQual valor deseja sacar?\nR$"))
+        if valor_saque2 > saldo_deposito:
+            print("Valor inválido. Por favor começar a operação novamente!") 
+        elif valor_saque2 < saldo_deposito:
+            print(f"Saque realizado com sucesso! Seu saldo atual é de R${saldo_deposito-valor_saque2:.2f}\nObrigado por utilizar nossos serviços.")
+    elif outras_operacoes == 3:
+         print(f"Seu saldo atual é de R${saldo_deposito:.2f}\nObrigado por utilizar nossos serviços.")
     #saque
-    if mensagem == "1":
-        saque = float(input("Qual valor deseja sacar?\n:R$"))
-        if saque > saldo_deposito:
-            mensagem_saque = input("Seu saldo é menor que o valor de saque. Deseja fazer um empréstimo?\n[1]- Sim [2]- Não\n:")
-            if mensagem_saque == "1":
-                #emprestimo
-                emprestimo = float(input("Qual o valor do empréstimo?\nR$:"))
-                saldo_com_emprestimo = saldo_deposito + emprestimo
-                mensagem_emprestimo = input(f"Seu saldo atual é de: R${saldo_com_emprestimo:.2f}!\nDeseja realizar um saque [1]- Sim [2]- Não\n:")
-                if mensagem_emprestimo== "1":
-                    saque_emprestimo = float(input("Qual valor deseja sacar?\nR$:"))
-                    saque_emprestimo_final = saldo_com_emprestimo - saque_emprestimo
-                    print(f"Saque realizado com sucesso! Seu saldo é de: R${saque_emprestimo_final:.2f}! Obrigado por utilizar nossos serviços. Tenha um ótimo dia!")
-                else:
-                    print(f"Seu saldo atual é de: R${saldo_com_emprestimo:.2f}!\nObrigado por utilizar nossos serviços. Tenha um ótimo dia!")     
-            else:
-                print("Obrigado por utilizar nossos serviços. Tenha um ótimo dia!")
-        else:
-            print(f"Saque realizado com sucesso! Seu saldo atual é de:R${saldo_deposito-saque:.2f}! Obrigado por utilizar nossos serviços. Tenha um ótimo dia!")  
-    else:
-        print(f"Seu saldo atual é de: R${saldo_deposito:.2f}!\nObrigado por utilizar nossos serviços. Tenha um ótimo dia!")
-    #saque feito 
-elif opcao =="2":
-    mensagem_saque2 = float(input("Qual valor deseja sacar?\nR$:"))
-    if mensagem_saque2 > saldo_atual:
-        print("Valor maior que seu saldo atual! Para prosseguir realize um depósito ou empréstimo.")
-    else:
-        valor_saque = saldo_atual - mensagem_saque2
-        print(f"Saque realizado com sucesso! Seu valor atual é de:R${valor_saque:.2f}!\nObrigado por utilizar nosso serviços. Tenha um ótimo dia!")
- #empréstimo  
+elif mensagem_inicial == 2:
+    valor_saque = float(input(f"Seu saldo atual é de R${saldo_inicial:.2f}!\nQual valor deseja sacar?\nR$")) 
+    if valor_saque > saldo_inicial:
+        print("Valor de saque maior que o saldo atual. Por favor começar a operação novamente.")
+    elif valor_saque < saldo_inicial:
+        saldo_saque = saldo_inicial - valor_saque
+        print(f"Saque realizado com sucesso! Seu saldo atual é de R${saldo_saque:.2f}\nDeseja realizar outra operação?")
+        outras_operacoes = int(input("[1]- Depósito [2]- Saque [3]- Sair\n"))
+        if outras_operacoes == 1:
+            valor_deposito3=float(input(f"Seu saldo atual é de R${saldo_saque:.2f}!\nQual valor deseja depositar?\nR$"))
+            if valor_deposito3 >0:
+                print(f"Depósito realizado com sucesso! Seu saldo atual é de {saldo_saque+valor_deposito3:.2f}\nObrigado por utilizar nossos serviços.")
+        elif outras_operacoes == 2:
+            valor_saque3 = float(input(f"Seu saldo atual é de R${saldo_saque:.2f}!\nQual valor deseja sacar?\nR$"))
+            if valor_saque3 > saldo_saque:
+                print("Valor de saque maior que o saldo atual. Por favor começar a operação novamente!") 
+            elif valor_saque3 < saldo_saque:
+                print(f"Saque realizado com sucesso! Seu saldo atual é de R${saldo_saque-valor_saque3:.2f}\nObrigado por utilizar nossos serviços.")
+        elif outras_operacoes == 3:
+         print(f"Seu saldo atual é de R${saldo_saque:.2f}\nObrigado por utilizar nossos serviços.")
+    #emprestimo
+elif mensagem_inicial == 3:
+    valor_emprestimo = float(input(f"Seu saldo atual é de R${saldo_inicial:.2f}!\nQual valor do empréstimo?\nR$"))
+    saldo_emprestimo = saldo_inicial+valor_emprestimo
+    print(f"Empréstimo realizado com sucesso! Seu saldo atual é de R${saldo_emprestimo:.2f}\nDeseja realizar outra operação?")
+    outras_operacoes = int(input("[1]- Depósito [2]- Saque [3]- Sair\n"))
+    if outras_operacoes == 1:
+        valor_deposito2=float(input(f"Seu saldo atual é de R${saldo_emprestimo:.2f}!\nQual valor deseja depositar?\nR$"))
+        if valor_deposito2 >0:
+            print(f"Depósito realizado com sucesso! Seu saldo atual é de {saldo_emprestimo+valor_deposito2:.2f}\nObrigado por utilizar nossos serviços.")
+    elif outras_operacoes == 2:
+        valor_saque2 = float(input(f"Seu saldo atual é de R${saldo_emprestimo:.2f}!\nQual valor deseja sacar?\nR$")) 
+        if valor_saque2 > saldo_emprestimo:
+            print("Valor de saque maior que saldo atual. Por favor começar a operação novamente.")
+        elif valor_saque2 < saldo_emprestimo:
+            print(f"Saque realizado com sucesso! Seu saldo atual é de R${saldo_emprestimo-valor_saque2:.2f}\nObrigado por utilizar nossos serviços.")
+    elif outras_operacoes == 3:
+         print(f"Seu saldo atual é de R${saldo_emprestimo:.2f}\nObrigado por utilizar nossos serviços.")
 else:
-    emprestimo_mensagem = input("Deseja prosseguir com o emprestimo?\n[1]- Sim [2]- Não\n:")
-    if emprestimo_mensagem =='1':
-        emprestimo2 = float(input("Qual o valor do empréstimo?\nR$:"))
-        saldo_com_emprestimo = saldo_atual + emprestimo2
-        mensagem_emprestimo = input(f"Seu saldo atual é de: R${saldo_com_emprestimo:.2f}!\nDeseja realizar um saque [1]- Sim [2]- Não\n:")
-        if mensagem_emprestimo== "1":
-                    saque_emprestimo = float(input("Qual valor deseja sacar?\nR$:"))
-                    saque_emprestimo_final = saldo_com_emprestimo - saque_emprestimo
-                    print(f"Saque realizado com sucesso! Seu saldo é de: R${saque_emprestimo_final:.2f}! Obrigado por utilizar nossos serviços. Tenha um ótimo dia!")
-        else:
-                    print(f"Seu saldo atual é de: R${saldo_com_emprestimo}!\nObrigado por utilizar nossos serviços. Tenha um ótimo dia!")
-    else:
-        print(f"Obrigado por utilizar nosso serviços. Seu saldo atual é de: R${saldo_atual:.2f}!\nTenha um ótimo dia!")
+    print("Opção não encontrada. Por favor comece a operação novamente!")
